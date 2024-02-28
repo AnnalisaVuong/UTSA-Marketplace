4aa7972672d8fa4aca6c2cb805898cba79e07ae0
Matthew <92887765+mjanderson1227@users.noreply.github.com>
2024-02-28 05:23:50 -0600

    Set up svelte boilerplate

---
+++ frontend/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from "./lib/Header.svelte";
+</script>
+
+<Header></Header>
+<main></main>
+
+<style>
+  :global(body) {
+    background-color: #0e1111;
+    margin: 0;
+  }
+</style>
---
+++ frontend/src/lib/Header.svelte
@@ -0,0 +1,33 @@
+<script>
+</script>
+
+<header>
+  <div>
+    <img src="/utsa-logo-sm.png" alt="University of Texas at San Antonio" />
+    <h1>Rowdy Marketplace</h1>
+  </div>
+</header>
+
+<style>
+  header {
+    display: flex;
+    background-color: inherit;
+    padding: 1.5em;
+    background-color: #0c2340;
+    color: #fff;
+    width: 100%;
+  }
+  header > div {
+    display: flex;
+    flex-direction: column;
+  }
+  img {
+    height: 60px;
+    width: 200px;
+  }
+  h1 {
+    margin: auto 0;
+    text-align: center;
+    font-size: 1.5rem;
+  }
+</style>
