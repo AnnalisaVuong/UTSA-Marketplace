195fc11412295f515691a4b1cdeea91877500e78
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-03-01 15:01:37 -0600

    Migrated svelte frontend over to react

---
+++ frontend/src/HeaderDropdown.jsx
@@ -0,0 +1,11 @@
+function HeaderDropdown() {
+  return (
+    <div>
+      <div class="dropdown" style="height: {$dropdownHeight}px">
+        <a href="/home">Home</a>
+        <a href="/features">Features</a>
+        <a href="/overview">Overview</a>
+      </div>
+    </div>
+  );
+}
--- frontend/src/lib/stores.js
+++
@@ -1,9 +0,0 @@
-// import { writable } from "svelte/store";
-import { tweened } from "svelte/motion";
-
-/* Data that needs to be shared across components will be stored here */
-
-/* Using the height of an element as a store for animations */
-export const dropdownHeight = tweened(0, {
-  duration: 200,
-});
