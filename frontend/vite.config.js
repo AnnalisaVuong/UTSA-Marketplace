645f7d1d86b7748f84e8e1671d07c47256f956b5
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-03-05 19:37:33 -0600

    Revamped project structure to support JSDoc

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // Create type aliases to manage folder structure.
     alias: {
+      "@assets": "/src/assets",
       "@components": "/src/components",
-      "@style": "/src/style",
+      "@hooks": "/src/hooks",
       "@lib": "/src/lib",
-      "@routes": "/src/routes/routes.jsx",
-      "@hooks": "/src/hooks/hooks.jsx",
+      "@routes": "/src/routes",
+      "@style": "/src/style",
     },
   },
 });
