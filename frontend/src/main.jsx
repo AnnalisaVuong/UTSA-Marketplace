7d69447afdf0209516991af58bb313c6dfb293ec
xxmatthewandersonxx <92887765+mjanderson1227@users.noreply.github.com>
2024-03-18 19:27:41 -0500

    Started progress on new components.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LandingPageRoute } from "@routes/routes";
-import { UserCreationRoute } from "@routes/CreateUser";
 import Admin from "@routes/AdminCreation";
+import { UserCreationRoute } from "@routes/CreateUser";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; <- To be added later when caching is desired.
 
 // Add new routes here.
