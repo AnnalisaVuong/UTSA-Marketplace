a28473eca1610961d2a5b4f4b4869423e419c3ac
fufupatwo <aaronadelgadoo@gmail.com>
2024-03-26 10:41:58 -0500

    setup endpoints for user/admin. - need to handle form evaluation

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,32 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 def create_app():
     app = Flask(__name__)
+
+
+    admin_bp = Blueprint('admin', __name__)
+
+    @admin_bp.route('/admin/create')
+    def admin_create():
+        return 'Admin Create Page'
+
+    @admin_bp.route('/admin/edit')
+    def admin_edit():
+        return 'Admin Edit Page'
+     
+    user_bp = Blueprint('user', __name__)
+
+    @user_bp.route('/user/create')
+    def user_create():
+        return 'User Create Page'
+
+    @user_bp.route('/user/edit')
+    def user_edit():
+        return 'User Edit Page'
+
+ 
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(user_bp)
 
 
     @app.route('/')
