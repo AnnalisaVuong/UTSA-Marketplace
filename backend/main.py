f580bc93051a2ae42baf5a883d67052fa4e72537
fufupatwo <aaronadelgadoo@gmail.com>
2024-03-28 15:35:06 -0500

    added admin create route

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ def create_app():
     app = Flask(__name__)
 
     user_bp = Blueprint('user', __name__)
+    admin_bp = Blueprint('admin', __name__)
 
     @user_bp.route('/user/create', methods=['POST'])
     def user_create():
@@ -23,11 +24,29 @@ def create_app():
          else:
             return jsonify({'error': 'Invalid JSON'}), 400
 
+    @admin_bp.route('/admin/create', methods=['POST'])
+    def admin_create():
+        if request.is_json:
+            data = request.json
+            username = data.get('username')
+            password = data.get('password')
+            first_name = data.get('first_name')
+            last_name = data.get('last_name')
+            email = data.get('email')
+            phone_number = data.get('phone_number')
+
+            
+            print(f"Received data: Username: {username}, Password: {password}, First Name: {first_name}, Last Name: {last_name}, Email: {email}, Phone Number: {phone_number}")
+
+            return jsonify({'message': 'User account created successfully'})
+        else:
+            return jsonify({'error': 'Invalid JSON'}), 400
 
 
 
  
     app.register_blueprint(user_bp)
+    app.register_blueprint(admin_bp)
 
 
     @app.route('/')
