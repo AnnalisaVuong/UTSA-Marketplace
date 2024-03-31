e87496d32c1714fc700827f703176c387bdb5939
AnnalisaVuong <108588878+AnnalisaVuong@users.noreply.github.com>
2024-03-31 16:04:58 -0500

    Complete tailwind migration for login and sign up page

--- frontend/src/routes/CreateAccount.jsx
+++ frontend/src/routes/CreateAccount.jsx
@@ -10,48 +10,155 @@ import "@style/Admin.css";
  *
  * @returns {React.ReactElement} Component to be rendered at the Create account page route.
  */
-export default function CreateAccount() {
 
+export default function Example() {
+    return (
+      <>
+        {}
+        <div className="flex min-h-full items-center flex-col justify-center px-6 py-5 lg:px-8">
+          <div className="flex w-96 p-6 shadow-lg bg-orange-500 rounded-md flex-col justify-center">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-9 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign in 
+              </h2>
+            </div>
+    
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-50">
+                    Username
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="username"
+                      name="username"
+                      type="username"
+                      autoComplete="username"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-50">
+                      Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
 
-return (
-    <>
-    <div className="container">
-        <h1 className="color header">Create Account </h1>
-        <form>
-            <div>
-                <label htmlFor="username" className="color bold">User Name</label>
-                <input type="text" id="username" name="username" className="inputStyling" required placeholder="Enter a User Name"/><br/>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="confirmed-password" className="block text-sm font-medium leading-6 text-gray-50">
+                      Confirm Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="confirm-password"
+                      name="confirm-password"
+                      type="confirm-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-50">
+                    Email
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="fName" className="block text-sm font-medium leading-6 text-gray-50">
+                    First Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="fName"
+                      name="fName"
+                      type="fName"
+                      autoComplete="fName"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="lName" className="block text-sm font-medium leading-6 text-gray-50">
+                    Last Name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="lName"
+                      name="lName"
+                      type="lName"
+                      autoComplete="lName"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-50">
+                    Phone Number
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="phone-number"
+                      name="phone-number"
+                      type="phone-number"
+                      autoComplete="phone-number"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Create Account
+                  </button>
+                </div>
+              </form>
+    
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Already a member?{' '}
+                <a href="/Login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Login
+                </a>
+              </p>
             </div>
-            <div>
-                <label htmlFor="pwd" className="color bold">Password</label>
-                <input type="password" id="password" name="pwd" className="inputStyling" required placeholder="Enter a Password"/><br/>
             </div>
-            <div>
-                <label htmlFor="pwd" className="color bold">Confirm Password</label>
-                <input type="password" id="confirm_password" name="pwd" className="inputStyling" required placeholder="Confirm Password"/><br/>
-            </div>
-            <div>
-                <label htmlFor="fname" className="color bold">First Name</label>
-                <input type="text" id="fname" name="fname" className="inputStyling" required placeholder="First Name"/><br/>
-            </div>
-            <div>
-                <label htmlFor="lname" className="color bold">Last Name</label>
-                <input type="text" id="lname" name="lname" className="inputStyling" required placeholder="Last Name"/><br/>
-            </div>
-            <div>
-                <label htmlFor="email" className="color bold">Email Address</label>
-                <input type="email" id="email" name="email" className="inputStyling" required placeholder="john@your-domain.com"/><br/>
-            </div>
-            <div>
-                <label htmlFor="phone" className="color bold">Phone Number</label>
-                <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" className="inputStyling" required placeholder="000-000-0000"/><br/>
-            </div>
-            <div>
-                <input type="submit" value="Create" className="bold big-text"/>
-                <input type="reset" value="Clear Form" className="bold big-text"/>
-            </div>
-        </form>
-    </div>
-    </>
-);
-}
+        </div>
+      </>
+    )
+  }
