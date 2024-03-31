912a19a3d97bab1cf8c5d3f7142b89fbe9080b79
Matthew <92887765+mjanderson1227@users.noreply.github.com>
2024-03-31 14:24:54 -0500

    Added Form Handling for CreateUser

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,16 +4,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { LandingPageRoute } from "@routes/routes";
 import { LoginPageRoute } from "@routes/routes";
 import { CreateAccountRoute } from "@routes/routes";
-// @ts-ignore
 import Admin from "@routes/AdminCreation";
 
 // Add new routes here.
 const router = createBrowserRouter([
   LandingPageRoute,
-{
+  {
     path: "admin/",
     element: <Admin />,
-}, LoginPageRoute, CreateAccountRoute,
+  },
+  LoginPageRoute,
+  CreateAccountRoute,
 ]);
 
 // Select root element for rendering ReactDOM tree.
--- frontend/src/routes/CreateAccount.jsx
+++ frontend/src/routes/CreateAccount.jsx
@@ -1,5 +1,6 @@
-import { Form } from "react-router-dom";
-import "@style/Admin.css";
+import { useState } from "react";
+
+const BACKEND_URL = "http://localhost:5000";
 
 /**
  * Route: Create Account Page
@@ -10,155 +11,218 @@ import "@style/Admin.css";
  *
  * @returns {React.ReactElement} Component to be rendered at the Create account page route.
  */
-
 export default function Example() {
-    return (
-      <>
-        {}
-        <div className="flex min-h-full items-center flex-col justify-center px-6 py-5 lg:px-8">
-          <div className="flex w-96 p-6 shadow-lg bg-orange-500 rounded-md flex-col justify-center">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <h2 className="mt-9 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in 
-              </h2>
-            </div>
-    
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form className="space-y-6" action="#" method="POST">
-                <div>
-                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-50">
-                    Username
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="username"
-                      name="username"
-                      type="username"
-                      autoComplete="username"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-50">
-                      Password
-                    </label>
-                  </div>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      autoComplete="current-password"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+  const [formData, setFormData] = useState({});
 
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label htmlFor="confirmed-password" className="block text-sm font-medium leading-6 text-gray-50">
-                      Confirm Password
-                    </label>
-                  </div>
-                  <div className="mt-2">
-                    <input
-                      id="confirm-password"
-                      name="confirm-password"
-                      type="confirm-password"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+  /** @param {React.FormEvent<HTMLFormElement>} event */
+  async function handleSubmit(event) {
+    event.preventDefault();
 
-                <div>
-                  <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-50">
-                    Email
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      autoComplete="email"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+    const formDataString = JSON.stringify(formData);
 
-                <div>
-                  <label htmlFor="fName" className="block text-sm font-medium leading-6 text-gray-50">
-                    First Name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="fName"
-                      name="fName"
-                      type="fName"
-                      autoComplete="fName"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+    const extraHeaders = new Headers();
+    extraHeaders.append("Accept", "application/json");
+    extraHeaders.append("Content-Type", "application/json");
 
-                <div>
-                  <label htmlFor="lName" className="block text-sm font-medium leading-6 text-gray-50">
-                    Last Name
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="lName"
-                      name="lName"
-                      type="lName"
-                      autoComplete="lName"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-50">
-                    Phone Number
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="phone-number"
-                      name="phone-number"
-                      type="phone-number"
-                      autoComplete="phone-number"
-                      required
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
+    const res = await fetch(BACKEND_URL + "/user/create", {
+      method: "POST",
+      headers: extraHeaders,
+      body: formDataString,
+    });
 
-                <div>
-                  <button
-                    type="submit"
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Create Account
-                  </button>
-                </div>
-              </form>
-    
-              <p className="mt-10 text-center text-sm text-gray-500">
-                Already a member?{' '}
-                <a href="/Login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                  Login
-                </a>
-              </p>
-            </div>
-            </div>
-        </div>
-      </>
-    )
+    const responseJson = await res.json();
+
+    console.log(responseJson);
   }
+
+  /** @param {React.ChangeEvent<HTMLInputElement>} event */
+  function handleFormChange(event) {
+    const escaped = event.target.name.replace("-", "_");
+    console.log(escaped);
+    setFormData({
+      ...formData,
+      [escaped]: event.target.value,
+    });
+  }
+
+  return (
+    <>
+      <div className="flex min-h-full items-center flex-col justify-center px-6 py-5 lg:px-8">
+        <div className="flex w-96 p-6 shadow-lg bg-orange-500 rounded-md flex-col justify-center">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-9 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+              Sign in
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium leading-6 text-gray-50"
+                >
+                  Username
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="username"
+                    name="username"
+                    type="username"
+                    autoComplete="username"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-50"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="confirmed-password"
+                    className="block text-sm font-medium leading-6 text-gray-50"
+                  >
+                    Confirm Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="confirm-password"
+                    name="confirm-password"
+                    type="confirm-password"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-50"
+                >
+                  Email
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-medium leading-6 text-gray-50"
+                >
+                  First Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="first-name"
+                    name="first-name"
+                    type="first-name"
+                    autoComplete="first-name"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-medium leading-6 text-gray-50"
+                >
+                  Last Name
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="last-name"
+                    name="last-name"
+                    type="last-name"
+                    autoComplete="last-name"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-medium leading-6 text-gray-50"
+                >
+                  Phone Number
+                </label>
+                <div className="mt-2">
+                  <input
+                    onChange={handleFormChange}
+                    id="phone-number"
+                    name="phone-number"
+                    type="phone-number"
+                    autoComplete="phone-number"
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Create Account
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-10 text-center text-sm text-gray-500">
+              Already a member?{" "}
+              <a
+                href="/Login"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                Login
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
