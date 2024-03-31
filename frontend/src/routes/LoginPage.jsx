4f7261d2015a4c24abdcf1db11e46a8ca632fa2b
AnnalisaVuong <108588878+AnnalisaVuong@users.noreply.github.com>
2024-03-31 16:04:58 -0500

    Complete tailwind migration for login and sign up page

--- frontend/src/routes/LoginPage.jsx
+++ frontend/src/routes/LoginPage.jsx
@@ -1,9 +1,8 @@
 import { Form, Link } from "react-router-dom";
-import "@style/Admin.css";
-import { useState } from "react";
 
 
-/**
+
+/** 
  * Route: Login Page
  *
  * URI: /
@@ -12,28 +11,74 @@ import { useState } from "react";
  *
  * @returns {React.ReactElement} Component to be rendered at the login page route.
  */
-export default function LoginPage() {
 
-
-return (
-    <>
-    <div className="container">
-        <h1 className="color header">Create Account </h1>
-        <form >
-            <div>
-                <label htmlFor="username" className="color bold">User Name</label>
-                <input type="text" id="username" name="username" className="inputStyling" required placeholder="Enter a User Name"/><br/>
+export default function Example() {
+    return (
+      <>
+        {}
+        <div className="flex min-h-full items-center flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="flex w-96 p-6 shadow-lg bg-orange-500 rounded-md flex-col justify-center">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign in 
+              </h2>
             </div>
-            <div>
-                <label htmlFor="pwd" className="color bold">Password</label>
-                <input type="password" id="password" name="pwd" className="inputStyling" required placeholder="Enter a Password"/><br/>
+    
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-50">
+                    Username
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="username"
+                      name="username"
+                      type="username"
+                      autoComplete="username"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-50">
+                      Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+    
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+    
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Not a member?{' '}
+                <a href="/CreateAccount" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                  Create Account
+                </a>
+              </p>
             </div>
-            <div>
-                <input type="submit" value="Login" className="bold big-text"/>
-                <Link to="/CreateAccount" className="px-8 py-2 rounded-lg hover:!border-orange-500 hover:text-orange-500">Create Acount</Link>
             </div>
-        </form>
-    </div>
-    </>
-);
-}
+        </div>
+      </>
+    )
+  }
