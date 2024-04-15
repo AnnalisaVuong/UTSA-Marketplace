6ea1ff0b6a9e7d37f35d68229848fa888dd004ad
Matthew <92887765+mjanderson1227@users.noreply.github.com>
2024-04-15 07:59:22 -0500

    Finished /post/create

--- backend/routes/listing_routes.py
+++ backend/routes/listing_routes.py
@@ -1,34 +1,43 @@
-from flask import jsonify, Blueprint, redirect, request, session, url_for
+from flask import jsonify, Blueprint, request, g
 from api_utils import jsonify_error, jwt_password
 from models import ItemListing, db
 from sqlalchemy import func
 import jwt
 
+
 bp = Blueprint("listing", __name__, url_prefix="/listing")
-validate_user = False
-
-
-# These routes will be protected by this authentication function.
-def authenticate() -> tuple[bool, dict | None]:
-    if "user_token" not in session:
-        return False, None
-
-    try:
-        data = jwt.decode(session["user_token"], jwt_password)
-    except KeyError or jwt.DecodeError:
-        return False, None
-
-    return True, data
 
 
 @bp.before_request
 def before():
-    if validate_user and not authenticate():
-        redirect(url_for("user_bp.user_login"))
+    invalid_response = jsonify_error(
+        err="Not Authorized",
+        msg="User does not have permission to access this resource",
+        status=401,
+    )
+
+    header = request.headers.get("Authorization")
+
+    if not header or not header.startswith("Bearer "):
+        return invalid_response
+
+    try:
+        token = header.split(" ")[1]
+        decoded = jwt.decode(token, jwt_password, ["HS256"])
+        setattr(g, "user_data", decoded)
+    except:
+        return invalid_response
 
 
 @bp.route("/create", methods=["POST"])
 def create():
+    if not (user_data := getattr(g, "user_data", None)):
+        return jsonify_error(
+            msg="User data not found.",
+            err="No user data",
+            status=401,
+        )
+
     if not request.is_json:
         return jsonify_error(
             msg="The mime type of the request is not application/json",
@@ -37,59 +46,76 @@ def create():
         )
 
     try:
-        new_listing = ItemListing()
         post_creation_data = request.get_json()
+        new_listing = ItemListing()
         new_listing.itemtitle = post_creation_data["title"]
         new_listing.itemdescription = post_creation_data["description"]
         new_listing.itemprice = post_creation_data["price"]
         new_listing.itemavailability = post_creation_data["available"]
         new_listing.itemposteddate = post_creation_data["date"]
+        new_listing.userid = user_data["userid"]
+        db.session.add(new_listing)
+        db.session.commit()
     except KeyError:
         return jsonify_error(
             msg="The shape of the creation data is invalid.",
             err="Malformed Request.",
             status=400,
         )
+    except:
+        return jsonify_error(
+            msg="An unknown error has occurred.",
+            err="Unknown Error",
+            status=400,
+        )
 
-    db.session.add(new_listing)
-    db.session.commit()
-
-    return jsonify({"message": "Post created successfully."})
+    return jsonify({"message": user_data})
 
 
-@bp.get("/retrieve")
-def retrieve():
-    posts_current_user = (
-        ItemListing.query.filter_by(userid=id).order_by(func.random()).limit(10)
-    )
+@bp.get("/retrieve/listings/<string:target>")
+def retrieve(target: str):
+    if not (request.authorization and (token_client := request.authorization.token)):
+        return jsonify_error(
+            err="Not Authorized",
+            msg="Client does not have access to this resource.",
+            status=401,
+        )
 
-    posts_other_users = (
-        ItemListing.query.filter(ItemListing.userid != id)
-        .order_by(func.random())
-        .limit(10)
-    )
+    try:
+        jwt_data = jwt.decode(token_client, jwt_password)
+        id = jwt_data["userid"]
+    except:
+        return jsonify_error(
+            err="JWT Parsing Error",
+            msg="An error occurred while parsing the jwt object.",
+            status=401,
+        )
 
-    return_listings = {
-        "listings_self": [
-            {
-                "title": post.itemtitle,
-                "desc": post.itemdescription,
-                "price": post.itemprice,
-                "available": post.itemavailability,
-                "data": post.itemposteddate,
-            }
-            for post in posts_current_user
-        ],
-        "listings_other": [
-            {
-                "title": post.itemtitle,
-                "desc": post.itemdescription,
-                "price": post.itemprice,
-                "available": post.itemavailability,
-                "data": post.itemposteddate,
-            }
-            for post in posts_other_users
-        ],
+    if target == "self":
+        items = ItemListing.query.filter(ItemListing.userid == id)
+    elif target.isdigit():
+        items = ItemListing.query.filter(ItemListing.userid == int(target))
+    else:
+        items = ItemListing.query.filter(ItemListing.userid != id)
+
+    if items.first():
+        post_list = items.order_by(func.random()).limit(10)
+    else:
+        return jsonify_error(
+            err="Nonexistent listings.",
+            msg="No listings currently exist.",
+            status=400,
+        )
+
+    listings_toreturn = {
+        post.userid: {
+            "title": post.itemtitle,
+            "desc": post.itemdescription,
+            "price": post.itemprice,
+            "available": post.itemavailability,
+            "data": post.itemposteddate,
+        }
+        for post in post_list
     }
 
-    return jsonify(return_listings)
+    return jsonify(listings_toreturn)
